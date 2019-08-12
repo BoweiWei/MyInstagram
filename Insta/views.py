9d61fe926e0f8f4b36695132a875b70aaf1ac0e3
BoweiWei <bowei_wei@brown.edu>
2019-08-11 20:39:37 -0400

    t

--- Insta/views.py
+++ Insta/views.py
@@ -61,7 +61,7 @@ class UserEdit(UpdateView):
     template_name = 'edit_profile.html'
     # only title is allowed to update at this point
     fields = ['profile_pic', 'email']
-    success_url = reverse_lazy('user_detail.html')
+    success_url = reverse_lazy('user_detail')
 
 class PostDeleteView(DeleteView):
     model = Post
